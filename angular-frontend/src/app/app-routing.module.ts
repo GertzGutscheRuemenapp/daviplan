8469ddacad953e47e88a1b50c71647519f573c2a
Christoph Franke <franke@ggr-planung.de>
2021-06-23 17:17:25 +0200

    moved users to admin folder

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdministrationComponent } from './administration/administration.component';
-import { UsersComponent } from './users/users.component';
+import { UsersComponent } from './administration/users/users.component';
 
 const routes: Routes = [
   {
