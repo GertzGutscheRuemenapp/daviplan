8b84c601a9dc58aa17fb9ef3ea24c05703631e19
Christoph Franke <franke@ggr-planung.de>
2021-07-22 11:45:09 +0200

    different APIs for dev and prod

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiPath: 'localhost:8000/api'
 };
 
 /*
