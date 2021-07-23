db80d5261051059f94cca0d376a73ae16fde68f8
Christoph Franke <franke@ggr-planung.de>
2021-07-23 13:06:17 +0200

    style fixes

--- angular-frontend/src/app/pages/planning/planning.component.ts
+++ angular-frontend/src/app/pages/planning/planning.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.component.html',
   styleUrls: ['./planning.component.scss']
 })
-export class PlanningComponent implements OnInit {
-  selectedItem: String = '';
+export class PlanningComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elRef: ElementRef) {  }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    // there is no parent css selector yet but we only want to hide the overflow in the planning pages
+    let wrapper = this.elRef.nativeElement.closest('mat-sidenav-container');
+    this.renderer.setStyle(wrapper.nativeElement, 'overflow', 'hidden');
   }
 
   public onMapReady(event:any) {
