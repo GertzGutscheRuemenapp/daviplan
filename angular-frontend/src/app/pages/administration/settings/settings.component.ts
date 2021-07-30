d1a57df4a619afdb1167434a9c55c797ab5a80a8
Christoph Franke <franke@ggr-planung.de>
2021-07-30 11:18:08 +0200

    wysiwyg html editor for welcome text

--- angular-frontend/src/app/pages/administration/settings/settings.component.ts
+++ angular-frontend/src/app/pages/administration/settings/settings.component.ts
@@ -3,6 +3,8 @@ import { SettingsService, SiteSettings } from "../../../settings.service";
 import { DataCardComponent } from "../../../dash/data-card.component";
 import { HttpClient } from "@angular/common/http";
 import { RestAPI } from "../../../rest-api";
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+import { set } from "ol/transform";
 
 @Component({
   selector: 'app-settings',
@@ -11,10 +13,19 @@ import { RestAPI } from "../../../rest-api";
 })
 export class SettingsComponent implements AfterViewInit {
   @ViewChild('appearanceCard') appearanceCard?: DataCardComponent;
+  @ViewChild('welcomeTextCard') welcomeTextCard?: DataCardComponent;
   settings?: SiteSettings;
-  primaryColor: string = '';
-  secondaryColor: string = '';
+  primaryColorInput: string = '';
+  secondaryColorInput: string = '';
+  welcomeTextInput: string = '';
   appearanceErrors: any = {};
+  welcomeTextErrors: any = {};
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: false,
+    height: '300px',
+    minHeight: '100px'
+  }
   // workaround to have access to object iteration in template
   Object = Object;
 
@@ -26,18 +37,18 @@ export class SettingsComponent implements AfterViewInit {
       this.settings = Object.assign({}, settings);
       this.cdRef.detectChanges();
       this.setupAppearanceCard();
+      this.setupWelcomeTextCard();
     });
   }
 
   setupAppearanceCard(): void {
     if (!this.settings || !this.appearanceCard) return;
-    this.primaryColor = this.settings.primaryColor;
-    this.secondaryColor = this.settings.secondaryColor;
+    this.primaryColorInput = this.settings.primaryColor;
+    this.secondaryColorInput = this.settings.secondaryColor;
     this.appearanceCard.dialogConfirmed.subscribe((ok)=>{
       let attributes: any = {
-        id: this.settings?.id,
-        primaryColor: this.primaryColor,
-        secondaryColor: this.secondaryColor
+        primaryColor: this.primaryColorInput,
+        secondaryColor: this.secondaryColorInput
       }
       this.appearanceCard?.setLoading(true);
       this.http.patch<SiteSettings>(this.rest.URLS.settings, attributes
@@ -51,20 +62,43 @@ export class SettingsComponent implements AfterViewInit {
     this.appearanceCard.dialogClosed.subscribe((ok)=>{
       // reset colors on cancel
       if (!ok){
-        this.primaryColor = this.settings!.primaryColor;
-        this.secondaryColor = this.settings!.secondaryColor;
-        this.appearanceErrors = {};
+        this.primaryColorInput = this.settings!.primaryColor;
+        this.secondaryColorInput = this.settings!.secondaryColor;
       }
-      this.settingsService.setColor({ primary: this.primaryColor, secondary: this.secondaryColor });
+      this.appearanceErrors = {};
+      this.settingsService.setColor({ primary: this.primaryColorInput, secondary: this.secondaryColorInput });
     });
   }
 
   onPrimaryChange(hex: string): void {
-    this.settingsService.setColor({ primary: this.primaryColor });
+    this.settingsService.setColor({ primary: this.primaryColorInput });
   }
 
   onSecondaryChange(hex: string): void {
-    this.settingsService.setColor({ secondary: this.secondaryColor });
+    this.settingsService.setColor({ secondary: this.secondaryColorInput });
+  }
+
+  setupWelcomeTextCard(): void {
+    if (!this.settings || !this.welcomeTextCard) return;
+    this.welcomeTextInput = this.settings.welcomeText;
+    this.welcomeTextCard.dialogConfirmed.subscribe((ok)=>{
+      let attributes: any = {
+        welcomeText: this.welcomeTextInput
+      }
+      this.welcomeTextCard?.setLoading(true);
+      this.http.patch<SiteSettings>(this.rest.URLS.settings, attributes
+      ).subscribe(settings => {
+        this.settings!.welcomeText = settings.welcomeText;
+        this.welcomeTextCard?.closeDialog(true);
+      },(error) => {
+        this.welcomeTextErrors = error.error;
+        this.welcomeTextCard?.setLoading(false);
+      });
+    });
+    this.welcomeTextCard.dialogClosed.subscribe((ok)=>{
+      this.welcomeTextErrors = {};
+      this.settingsService.setColor({ primary: this.primaryColorInput, secondary: this.secondaryColorInput });
+    });
   }
 
 }
