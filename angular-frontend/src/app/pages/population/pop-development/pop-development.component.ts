80b1f2619d3f22b802093ed38efd6c6a700b465b
Christoph Franke <franke@ggr-planung.de>
2021-08-05 18:40:17 +0200

    barchart test

--- angular-frontend/src/app/pages/population/pop-development/pop-development.component.ts
+++ angular-frontend/src/app/pages/population/pop-development/pop-development.component.ts
@@ -1,6 +1,13 @@
-import {Component, AfterViewInit, OnDestroy} from '@angular/core';
-import {MapService} from "../../../map/map.service";
-import {OlMap} from "../../../map/map";
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { MapService } from "../../../map/map.service";
+import { OlMap } from "../../../map/map";
+import { BarData } from "../../../diagrams/stacked-barchart/stacked-barchart.component";
+
+const mockdata: BarData[] = [
+  { year: 2000, count: 100 },
+  { year: 2001, count: 200 },
+  { year: 2002, count: 300 },
+]
 
 @Component({
   selector: 'app-pop-development',
@@ -9,6 +16,7 @@ import {OlMap} from "../../../map/map";
 })
 export class PopDevelopmentComponent implements AfterViewInit, OnDestroy {
   map?: OlMap;
+  data: BarData[] = mockdata;
 
   constructor(private mapService: MapService) { }
 
