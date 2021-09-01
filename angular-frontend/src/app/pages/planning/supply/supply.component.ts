92c7885efa2a921c7e8b846d9fbda025fbd54fa1
Christoph Franke <franke@ggr-planung.de>
2021-09-01 14:19:53 +0200

    supply page menu; planning side menu style; removed map controls by default

--- angular-frontend/src/app/pages/planning/supply/supply.component.ts
+++ angular-frontend/src/app/pages/planning/supply/supply.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
-import {map} from "rxjs/operators";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-supply',
@@ -8,28 +7,8 @@ import {map} from "rxjs/operators";
   styleUrls: ['./supply.component.scss']
 })
 export class SupplyComponent implements OnInit{
-
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  activeInfrastructure: string = '';
+  constructor() {}
 
   ngOnInit(): void {
   }
