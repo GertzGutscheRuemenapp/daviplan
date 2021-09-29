2159f3efbf57a58486305a816db0ad22dec491fa
MaxBo <bohnet@ggr-planung.de>
2021-09-29 19:26:37 +0200

    added last admin.pys

--- datentool_backend/admin.py
+++ datentool_backend/admin.py
@@ -2,3 +2,6 @@ import datentool_backend.area.admin
 import datentool_backend.indicators.admin
 import datentool_backend.logging.admin
 import datentool_backend.infrastructure.admin
+import datentool_backend.population.admin
+import datentool_backend.site.admin
+import datentool_backend.user.admin
