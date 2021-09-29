1c356d05148148954868811d8e9a9eb11dda48d3
Christoph Franke <franke@ggr-planung.de>
2021-09-29 16:12:40 +0200

    new login and welcome page style

--- angular-frontend/src/app/pages/planning/supply/supply.component.ts
+++ angular-frontend/src/app/pages/planning/supply/supply.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from "@angular/forms";
+import { MatDatepicker } from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-supply',
@@ -8,10 +9,17 @@ import { FormControl } from "@angular/forms";
 })
 export class SupplyComponent implements OnInit{
   activeInfrastructure: string = '';
+  compareYear = 2025;
   addPlaceMode = false;
+  @ViewChild('compareYearPicker', {static: false}) private compareYearPicker?: MatDatepicker<Date>;
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  chosenYearHandler(ev: any): void {
+    this.compareYearPicker?.close();
+    this.compareYear = ev.getFullYear();
   }
 }
