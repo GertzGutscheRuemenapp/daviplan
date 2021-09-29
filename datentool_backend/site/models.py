a54699e475e2d426b81190ddf8d9eaa3be15da52
MaxBo <bohnet@ggr-planung.de>
2021-09-29 19:26:24 +0200

    rename SiteSettings to SiteSetting

--- datentool_backend/site/models.py
+++ datentool_backend/site/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class SiteSettings(models.Model):
+class SiteSetting(models.Model):
     ''''''
     name = models.CharField(max_length=50, unique=True)
     title = models.TextField(default='Datentool')
