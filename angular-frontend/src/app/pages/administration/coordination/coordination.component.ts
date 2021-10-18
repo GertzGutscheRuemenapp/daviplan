1760a6967ff836b6194e4d9252888c2ddb33d814
Christoph Franke <franke@ggr-planung.de>
2021-10-18 17:18:54 +0200

    functionality toggle marker

--- angular-frontend/src/app/pages/administration/coordination/coordination.component.ts
+++ angular-frontend/src/app/pages/administration/coordination/coordination.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { User, mockUsers } from "../../login/users";
 
 interface DataDomain {
-  name: string, url: string, users: User[], status: {text: String, detail: String}
+  name: string,
+  url: string,
+  users: User[],
+  status: { text: string, detail: string },
+  marker?: number
 }
 
 const data: { [name: string]: DataDomain[] } = {
   'geodaten' : [
-    { name: 'Gebietseinheit', url: '/grundlagendaten/gebiete', users: [mockUsers[0]], status: { text: '3 Ebenen definiert', detail:'' }},
+    { name: 'Gebietseinheit', url: '/grundlagendaten/gebiete', users: [mockUsers[0]], status: { text: '3 Ebenen definiert', detail:'' }, marker: 1 },
     { name: 'Einwohnerraster', url: '/grundlagendaten/einwohnerraster', users: [mockUsers[0]], status: { text: 'Zensus 2011', detail:'' }},
-    { name: 'Regionalstatistik', url: '/grundlagendaten/statistiken', users: [], status: { text: 'Importiert', detail:'' }},
+    { name: 'Regionalstatistik', url: '/grundlagendaten/statistiken', users: [], status: { text: 'Importiert', detail:'' }, marker: 1 },
     { name: 'Externe Layer', url: '/grundlagendaten/layer', users: mockUsers, status: { text: '3 Layer definiert', detail:'' }}
   ],
   'einwohnerdaten' : [
-    { name: 'Realdaten', url: '/grundlagendaten/realdaten', users: [mockUsers[1]], status: { text: '5 Jahre definiert', detail:'' }},
-    { name: 'Prognosedaten', url: '/grundlagendaten/prognosedaten', users: [mockUsers[1]], status: { text: '-', detail:'' }}
+    { name: 'Realdaten', url: '/grundlagendaten/realdaten', users: [mockUsers[1]], status: { text: '5 Jahre definiert', detail:'' }, marker: 2},
+    { name: 'Prognosedaten', url: '/grundlagendaten/prognosedaten', users: [mockUsers[1]], status: { text: '-', detail:'' }, marker: 3}
   ]
 }
 
