cad36b2783bb28d126a5cba026791c80cd47e217
Christoph Franke <franke@ggr-planung.de>
2021-10-22 14:38:18 +0200

    first draft external layers page with tree select

--- angular-frontend/src/app/pages/basedata/external-layers/external-layers.component.ts
+++ angular-frontend/src/app/pages/basedata/external-layers/external-layers.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./external-layers.component.scss']
 })
 export class ExternalLayersComponent implements OnInit {
+  layerGroups = [
+    {name: 'Ökologie', layers: [{id: 1, name: 'Naturschutzgebiete'}, {id: 2, name: 'Wälder'}, {id: 8, name: 'unzerschnittene Freiräume'}, {id: 8, name: 'Wasserschutzgebiete'}]},
+    {name: 'Verkehr', layers: [{id: 3, name: 'ÖPNV'}, {id: 4, name: 'Lärmkarte'}, {id: 5, name: 'Fahrradwege'}]},
+    {name: 'Infrastruktur', layers: [{id: 6, name: 'Hochspannungsmasten'}, {id: 7, name: 'ALKIS Gebäudedaten'}]}
+  ]
 
   constructor() { }
 
