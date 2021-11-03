622513ae358117b5851787224e955c14eb3203e0
Christoph Franke <franke@ggr-planung.de>
2021-11-03 17:38:41 +0100

    overhaul process and indicator selection panel

--- angular-frontend/src/app/pages/planning/rating/rating.component.ts
+++ angular-frontend/src/app/pages/planning/rating/rating.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { mockIndicators } from "../../basedata/indicators/indicators.component";
+import { Indicator, mockIndicators } from "../../basedata/indicators/indicators.component";
 import { mockPresetLevels } from "../../basedata/areas/areas";
 import { CookieService } from "../../../helpers/cookies.service";
+import { mockInfrastructures } from "../../administration/infrastructure/infrastructure.component";
+
+const mockCustomIndicators: Indicator[] = [
+  {id: 6, service: mockInfrastructures[0].services[0].id, name: 'Test + langer Text der die maximale Größe des Containers überschreiten sollte', description: 'eigener Testindikator'},
+  {id: 7, service: mockInfrastructures[0].services[0].id, name: 'zweiter Indikator', description: 'noch ein eigener Testindikator'},
+]
 
 @Component({
   selector: 'app-rating',
@@ -14,11 +20,17 @@ export class RatingComponent implements OnInit {
   compareStatus = 'option 1';
   indicators = mockIndicators;
   areaLevels = mockPresetLevels;
+  customIndicators = mockCustomIndicators;
   showScenarioMenu: any = false;
+  activeIndicator = mockIndicators[0];
 
   constructor(public cookies: CookieService) {}
 
   ngOnInit(): void {
     this.showScenarioMenu = this.cookies.get('exp-planning-scenario');
+  }
+
+  onIndicatorChange(id: number): void {
+    this.activeIndicator = this.indicators.concat(this.customIndicators).filter(ind => ind.id === id)[0];
   }
 }
