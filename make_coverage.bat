5b538286b47d09ea221539237a462925ed053936
MaxBo <bohnet@ggr-planung.de>
2021-11-08 11:33:49 +0100

    cd to path of make_covegage.bat

--- make_coverage.bat
+++ make_coverage.bat
@@ -1,3 +1,4 @@
 CALL conda activate datentool
+CALL cd %~dp0
 CALL coverage run --source='.' manage.py test datentool_backend --settings=datentool.settings_test
-CALL coverate html
+CALL coverage html
