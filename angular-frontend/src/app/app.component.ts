629d631a9789328b1ec89975c799c67c54b3e0a7
Christoph Franke <franke@ggr-planung.de>
2021-11-29 11:37:23 +0100

    close all dialogs on route change

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { SettingsService } from "./settings.service";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -8,11 +11,14 @@ import { SettingsService } from "./settings.service";
 })
 export class AppComponent {
 
-  constructor(private settings: SettingsService) {
+  constructor(private settings: SettingsService, router: Router, dialog: MatDialog) {
     // auto apply settings when new ones were fetched
     // ToDo: is this the right place for this?
     settings.siteSettings$.subscribe(settings => {
       this.settings.applySettings(settings);
     })
+    router.events.pipe(
+      tap(() => dialog.closeAll())
+    ).subscribe();
   }
 }
