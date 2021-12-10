ec0c27d4046e5cfd8346e2c069a9fe132a021b16
Christoph Franke <franke@ggr-planung.de>
2021-12-10 12:47:22 +0100

    fixed merge

--- datentool_backend/site/views.py
+++ datentool_backend/site/views.py
@@ -3,16 +3,16 @@ from rest_framework import viewsets, permissions
 from .models import SiteSetting, ProjectSetting, BaseDataSetting
 from .serializers import (SiteSettingSerializer, ProjectSettingSerializer,
                           BaseDataSettingSerializer)
-#from datentool_backend.utils.views import SingletonViewSet
+from datentool_backend.utils.views import SingletonViewSet
 
 
-class ProjectSettingViewSet(viewsets.ModelViewSet):
+class ProjectSettingViewSet(SingletonViewSet):
     queryset = ProjectSetting.objects.all()
     model_class = ProjectSetting
     serializer_class = ProjectSettingSerializer
 
 
-class BaseDataSettingViewSet(viewsets.ModelViewSet):
+class BaseDataSettingViewSet(SingletonViewSet):
     queryset = BaseDataSetting.objects.all()
     model_class = BaseDataSetting
     serializer_class = BaseDataSettingSerializer
