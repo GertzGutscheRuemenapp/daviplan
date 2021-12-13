659a18b9278592a91192dbaad650650b44c13d51
Sophie <luettger@ggr-planung.de>
2021-12-13 14:47:14 +0100

    add read_only permission to logging views, add PermissionMixin

--- datentool_backend/logging/tests.py
+++ datentool_backend/logging/tests.py
@@ -31,20 +31,136 @@ class TestLogs(TestCase):
         log = AreaUploadLogFactory()
 
 
-class TestCapacityUploadLogAPI(_TestAPI, BasicModelReadTest, APITestCase):
+class _TestReadOnly():
+    """Test if user can read_only if attributes can_edit_basedata or has_admin_access are true"""
+    def read_only_basedata(self):
+        profile = self.profile
+
+        original_permission = profile.can_edit_basedata
+
+        # Testprofile, with permission to edit basedata
+        profile.can_edit_basedata = True
+        profile.save()
+
+        url = self.url_key + '-list'
+        # post
+        response = self.post(url, **self.url_pks, data=self.post_data,
+                                 extra={'format': 'json'})
+        self.response_405(msg=response.content)
+
+        # Testprofile, without permission to edit basedata
+        profile.can_edit_basedata = False
+        profile.save()
+
+        url = self.url_key + '-list'
+        # post
+        response = self.post(url, **self.url_pks, data=self.post_data,
+                                 extra={'format': 'json'})
+        self.response_405(msg=response.content)
+
+        profile.can_edit_basedata = original_permission
+        profile.save()
+
+    def read_only_admin_access(self):
+        profile = self.profile
+
+        original_permission = profile.admin_access
+
+        # Testprofile, with admin_acces
+        profile.admin_access = True
+        profile.save()
+
+        url = self.url_key + '-list'
+        # post
+        response = self.post(url, **self.url_pks, data=self.post_data,
+                                 extra={'format': 'json'})
+        self.response_405(msg=response.content)
+
+        # Testprofile, without admin_acces
+        profile.admin_access = False
+        profile.save()
+
+        url = self.url_key + '-list'
+        # post
+        response = self.post(url, **self.url_pks, data=self.post_data,
+                                 extra={'format': 'json'})
+        self.response_405(msg=response.content)
+
+        profile.admin_access = original_permission
+        profile.save()
+
+
+class TestCapacityUploadLogAPI(_TestReadOnly, _TestAPI, BasicModelReadTest, APITestCase):
     """"""
     url_key = "capacityuploadlogs"
     factory = CapacityUploadLogFactory
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        capacityuploadlog: CapacityUploadLog = cls.obj
+        service = capacityuploadlog.service.pk
+        user = capacityuploadlog.user.pk
 
-class TestPlaceUploadLogAPI(_TestAPI, BasicModelReadTest, APITestCase):
+        data = dict(user=user, date = faker.date(), text = faker.sentence(),
+                    service=service)
+        cls.post_data = data
+        cls.put_data = data
+        cls.patch_data = data
+
+    def test_read_only_basedata(self):
+        super().read_only_basedata()
+
+    def test_read_only_admin_access(self):
+        super().read_only_admin_access()
+
+
+
+class TestPlaceUploadLogAPI(_TestReadOnly, _TestAPI, BasicModelReadTest, APITestCase):
     """"""
     url_key = "placeuploadlogs"
     factory = PlaceUploadLogFactory
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        placeuploadlog: PlaceUploadLog = cls.obj
+        infrastructure = placeuploadlog.infrastructure.pk
+        user = placeuploadlog.user.pk
 
-class TestAreaUploadLogAPI(_TestAPI, BasicModelReadTest, APITestCase):
+        data = dict(user=user, date = faker.date(), text = faker.sentence(),
+                    infrastructure=infrastructure)
+        cls.post_data = data
+        cls.put_data = data
+        cls.patch_data = data
+
+    def test_read_only_basedata(self):
+        super().read_only_basedata()
+
+    def test_read_only_admin_access(self):
+        super().read_only_admin_access()
+
+
+class TestAreaUploadLogAPI(_TestReadOnly, _TestAPI, BasicModelReadTest, APITestCase):
     """"""
     url_key = "areauploadlogs"
     factory = AreaUploadLogFactory
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        areauploadlog: AreaUploadLog = cls.obj
+        level = areauploadlog.level.pk
+        user = areauploadlog.user.pk
+
+        data = dict(user=user, date = faker.date(), text = faker.sentence(),
+                    level=level)
+        cls.post_data = data
+        cls.put_data = data
+        cls.patch_data = data
+
+    def test_read_only_basedata(self):
+        super().read_only_basedata()
+
+    def test_read_only_admin_access(self):
+        super().read_only_admin_access()
--- datentool_backend/logging/views.py
+++ datentool_backend/logging/views.py
@@ -1,20 +1,21 @@
 from rest_framework import viewsets
+from datentool_backend.utils.views import ReadOnlyEditBasedataAdminAccess
 
 from .models import (CapacityUploadLog, PlaceUploadLog, AreaUploadLog)
 from .serializers import (CapacityUploadLogSerializer, PlaceUploadLogSerializer,
                           AreaUploadLogSerializer)
 
 
-class CapacityUploadLogViewSet(viewsets.ReadOnlyModelViewSet):
+class CapacityUploadLogViewSet(ReadOnlyEditBasedataAdminAccess, viewsets.ReadOnlyModelViewSet):
     queryset = CapacityUploadLog.objects.all()
     serializer_class = CapacityUploadLogSerializer
 
 
-class PlaceUploadLogViewSet(viewsets.ReadOnlyModelViewSet):
+class PlaceUploadLogViewSet(ReadOnlyEditBasedataAdminAccess, viewsets.ReadOnlyModelViewSet):
     queryset = PlaceUploadLog.objects.all()
     serializer_class = PlaceUploadLogSerializer
 
 
-class AreaUploadLogViewSet(viewsets.ReadOnlyModelViewSet):
+class AreaUploadLogViewSet(ReadOnlyEditBasedataAdminAccess, viewsets.ReadOnlyModelViewSet):
     queryset = AreaUploadLog.objects.all()
     serializer_class = AreaUploadLogSerializer
