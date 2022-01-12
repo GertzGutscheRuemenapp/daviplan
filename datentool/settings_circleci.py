ca35a5509a635888de7718b6b86cfdfeb0d5cf0e
Christoph Franke <franke@ggr-planung.de>
2022-01-12 12:38:52 +0100

    new circleci test settings

---
+++ datentool/settings_circleci.py
@@ -0,0 +1,12 @@
+from datentool.settings_dev import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'circle_test',
+        'USER': 'ubuntu',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
