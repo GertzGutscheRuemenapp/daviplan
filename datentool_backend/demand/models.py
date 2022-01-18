1f79e3772ea5686013acf3e805ec9a37d18c0748
Sophie <luettger@ggr-planung.de>
2022-01-18 15:28:50 +0100

    Permission for Demand models

--- datentool_backend/demand/models.py
+++ datentool_backend/demand/models.py
@@ -5,6 +5,7 @@ from datentool_backend.infrastructure.models import (Service)
 from datentool_backend.user.models import Scenario
 from datentool_backend.utils.protect_cascade import PROTECT_CASCADE
 from datentool_backend.base import NamedModel, DatentoolModelMixin
+from datentool_backend.utils.views import (CanEditBasedata, HasAdminAccessOrReadOnly)
 
 
 class DemandRateSet(DatentoolModelMixin, NamedModel, models.Model):
@@ -12,6 +13,7 @@ class DemandRateSet(DatentoolModelMixin, NamedModel, models.Model):
     name = models.TextField()
     is_default = models.BooleanField()
     service = models.ForeignKey(Service, on_delete=PROTECT_CASCADE)
+    permission_classes = [HasAdminAccessOrReadOnly | CanEditBasedata]
 
 
 class DemandRate(models.Model):
@@ -20,7 +22,10 @@ class DemandRate(models.Model):
     age_group = models.ForeignKey(AgeGroup, on_delete=PROTECT_CASCADE)
     demand_rate_set = models.ForeignKey(DemandRateSet, on_delete=PROTECT_CASCADE)
     value = models.FloatField(null=True)
+    permission_classes = [HasAdminAccessOrReadOnly | CanEditBasedata]
 
 
 class ScenarioDemandRate(DemandRate):
     scenario = models.ForeignKey(Scenario, on_delete=PROTECT_CASCADE)
+    permission_classes = [HasAdminAccessOrReadOnly | CanEditBasedata]
+
