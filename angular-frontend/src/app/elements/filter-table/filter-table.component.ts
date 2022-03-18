6e11b2e20f7b93514f8578a9beab6d8a69eafe18
Christoph Franke <franke@ggr-planung.de>
2022-03-18 18:33:43 +0100

    place-filter - sorting wip

--- angular-frontend/src/app/elements/filter-table/filter-table.component.ts
+++ angular-frontend/src/app/elements/filter-table/filter-table.component.ts
@@ -12,11 +12,29 @@ export interface Header {
 export class FilterTableComponent implements OnInit {
   @Input() header!: string[];
   @Input() rows!: any[][];
-  filtered = [true, false, false, true, false, false];
+  filtered = [false, false, false, false, false, false];
+  // sorting: ('asc' | 'desc' | 'none')[];
+  sorting: ('asc' | 'desc' | 'none' )[] = [];
 
-  constructor() { }
+  constructor() {
+  }
+
+  toggleSort(column: number) {
+    const prevOrder = this.sorting[column];
+    // const order = (prevOrder === 'none')? 'asc': (prevOrder === 'asc')? 'desc': 'none';
+    const order = (prevOrder === 'asc')? 'desc': 'asc';
+    this.sorting[column] = order;
+    // if (order === 'none') return;
+    this.rows.sort((a, b) => {
+      if (a[column] === b[column]) return 0;
+      if (order === 'asc' && a[column] > b[column]) return 1;
+      if (order === 'desc' && a[column] < b[column]) return 1;
+      return -1;
+    })
+  }
 
   ngOnInit(): void {
+    this.sorting = Array(this.header.length).fill('none');
   }
 
 }
