cf797a668897b342ff824bd781f656155946c539
Christoph Franke <franke@ggr-planung.de>
2022-04-27 11:42:33 +0200

    page real data - refresh data on pull

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -194,7 +194,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
