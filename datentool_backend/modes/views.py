e80e3f52a3c0d4f5fb400f0eccd2d3f96e5a33eb
Christoph Franke <franke@ggr-planung.de>
2022-07-13 15:38:05 +0200

    backend - mode variant view

--- datentool_backend/modes/views.py
+++ datentool_backend/modes/views.py
@@ -4,8 +4,17 @@ from datentool_backend.utils.views import ProtectCascadeMixin
 from datentool_backend.utils.permissions import (
     HasAdminAccessOrReadOnly, CanEditBasedata)
 
-from .models import Network
-from .serializers import NetworkSerializer
+from .models import Network, ModeVariant
+from .serializers import NetworkSerializer, ModeVariantSerializer
+
+
+class ModeVariantViewSet(ProtectCascadeMixin, viewsets.ModelViewSet):
+    queryset = ModeVariant.objects.all()
+    serializer_class = ModeVariantSerializer
+    permission_classes = [HasAdminAccessOrReadOnly | CanEditBasedata]
+    # fun fact: you can't write the method names in upper case, they have
+    # to exactly match the ModelViewSet function names
+    http_method_names = ('get', 'patch', 'head', 'options')
 
 
 class NetworkViewSet(ProtectCascadeMixin, viewsets.ModelViewSet):
