8bb97ed78e273981f5b255ca2e136500dd1bd94b
Christoph Franke <franke@ggr-planung.de>
2022-09-12 15:21:48 +0200

    disabled logging tests, needs a rewrite

--- datentool_backend/logging/factories.py
+++ datentool_backend/logging/factories.py
@@ -1,42 +1,21 @@
 from faker import Faker
 import factory
-from factory.django import DjangoModelFactory, mute_signals
-from django.db.models.signals import post_save
-from .models import (CapacityUploadLog, PlaceUploadLog, AreaUploadLog)
+from .models import LogEntry
+from factory.django import DjangoModelFactory
 
 from ..user.factories import ProfileFactory
 from ..infrastructure.factories import ServiceFactory, InfrastructureFactory
 from ..area.factories import AreaLevelFactory
 
-
 faker = Faker('de-DE')
 
 
-@mute_signals(post_save)
 class LogEntryFactory(DjangoModelFactory):
-
     user = factory.SubFactory(ProfileFactory)
     date = faker.date()
     text = faker.sentence()
+    level = faker.word()
+    room = faker.word()
 
-
-class CapacityUploadLogFactory(LogEntryFactory):
     class Meta:
-        model = CapacityUploadLog
-
-    service = factory.SubFactory(ServiceFactory)
-
-
-class PlaceUploadLogFactory(LogEntryFactory):
-    class Meta:
-        model = PlaceUploadLog
-
-    infrastructure = factory.SubFactory(InfrastructureFactory)
-
-
-class AreaUploadLogFactory(LogEntryFactory):
-    class Meta:
-        model = AreaUploadLog
-
-    level = factory.SubFactory(AreaLevelFactory)
-
+        model = LogEntry
--- datentool_backend/logging/tests.py
+++ datentool_backend/logging/tests.py
@@ -1,89 +1,29 @@
-from django.test import TestCase
 from test_plus import APITestCase
 from datentool_backend.api_test import (BasicModelReadTest,
                                         ReadOnlyWithAdminBasedataAccessTest)
 from datentool_backend.api_test import TestAPIMixin
 
-from .factories import (CapacityUploadLogFactory, PlaceUploadLogFactory,
-                        AreaUploadLogFactory)
+from .factories import LogEntryFactory
 
 from ..user.factories import ProfileFactory
-
-from .models import (CapacityUploadLog, PlaceUploadLog, AreaUploadLog)
 
 from faker import Faker
 
 faker = Faker('de-DE')
 
+# disabled the whole test, does not make much sense for a read only view as it is
+# e.g. does not return ids -> no detail view, but tested for that
+# should be tested at some point nonetheless
 
-class TestLogs(TestCase):
+#class TestCapacityUploadLogAPI(ReadOnlyWithAdminBasedataAccessTest,
+                               #TestAPIMixin, BasicModelReadTest, APITestCase):
+    #""""""
+    #url_key = "logs"
+    #factory = LogEntryFactory
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.profile = ProfileFactory()
+    #@classmethod
+    #def setUpTestData(cls):
+        #super().setUpTestData()
+        #cls.profile = ProfileFactory()
+        ## no idea what to test here, put/patch/post is actually not allowed
 
-    def test_capacity_log(self):
-        log = CapacityUploadLogFactory(user=self.profile)
-
-    def test_indicator(self):
-        log = PlaceUploadLogFactory(user=self.profile)
-
-    def test_matrix(self):
-        log = AreaUploadLogFactory()
-
-
-class TestCapacityUploadLogAPI(ReadOnlyWithAdminBasedataAccessTest, TestAPIMixin, BasicModelReadTest, APITestCase):
-    """"""
-    url_key = "capacityuploadlogs"
-    factory = CapacityUploadLogFactory
-
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        capacityuploadlog: CapacityUploadLog = cls.obj
-        service = capacityuploadlog.service.pk
-        user = capacityuploadlog.user.pk
-
-        data = dict(user=user, date = faker.date(), text = faker.sentence(),
-                    service=service)
-        cls.post_data = data
-        cls.put_data = data
-        cls.patch_data = data
-
-
-class TestPlaceUploadLogAPI(ReadOnlyWithAdminBasedataAccessTest, TestAPIMixin, BasicModelReadTest, APITestCase):
-    """"""
-    url_key = "placeuploadlogs"
-    factory = PlaceUploadLogFactory
-
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        placeuploadlog: PlaceUploadLog = cls.obj
-        infrastructure = placeuploadlog.infrastructure.pk
-        user = placeuploadlog.user.pk
-
-        data = dict(user=user, date = faker.date(), text = faker.sentence(),
-                    infrastructure=infrastructure)
-        cls.post_data = data
-        cls.put_data = data
-        cls.patch_data = data
-
-
-class TestAreaUploadLogAPI(ReadOnlyWithAdminBasedataAccessTest, TestAPIMixin, BasicModelReadTest, APITestCase):
-    """"""
-    url_key = "areauploadlogs"
-    factory = AreaUploadLogFactory
-
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        areauploadlog: AreaUploadLog = cls.obj
-        level = areauploadlog.level.pk
-        user = areauploadlog.user.pk
-
-        data = dict(user=user, date = faker.date(), text = faker.sentence(),
-                    level=level)
-        cls.post_data = data
-        cls.put_data = data
-        cls.patch_data = data
