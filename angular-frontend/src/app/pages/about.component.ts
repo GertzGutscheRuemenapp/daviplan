22c706c71656867d300645d075cf89735186b355
Sophie <luettger@ggr-planung.de>
2022-09-28 16:03:32 +0200

    improved legal notice

--- angular-frontend/src/app/pages/about.component.ts
+++ angular-frontend/src/app/pages/about.component.ts
@@ -3,7 +3,7 @@ import { environment } from "../../environments/environment";
 
 @Component({
   templateUrl: './about.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss', './about.component.scss']
 })
 export class AboutComponent implements OnInit {
   backend: string = environment.backend;
