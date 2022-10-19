c80cef87d08e67c208ab3b04e6267e0d144a013d
Christoph Franke <franke@ggr-planung.de>
2022-10-19 11:03:42 +0200

    fixed area annotations

--- datentool_backend/area/models.py
+++ datentool_backend/area/models.py
@@ -156,11 +156,20 @@ class Area(DatentoolModelMixin, models.Model):
                                                  .values('_value')[:1])
                        for area_field in area_fields}
 
-        label_attribute = area_attributes.filter(is_label=True)
-        key_attribute = area_attributes.filter(is_key=True)
+        try:
+            label_field = area_fields.get(is_label=True)
+            label_attribute = area_attributes.filter(field=label_field)
+            annotations['_label'] = Subquery(label_attribute.values('_value')[:1])
+        except AreaField.DoesNotExist:
+            pass
 
-        annotations['_label'] = Subquery(label_attribute.values('_value')[:1])
-        annotations['_key'] = Subquery(key_attribute.values('_value')[:1])
+        try:
+            key_field = area_fields.get(is_key=True)
+            key_attribute = area_attributes.filter(field=key_field)
+            annotations['_key'] = Subquery(key_attribute.values('_value')[:1])
+        except AreaField.DoesNotExist:
+            pass
+
         annotations['_field_names'] = Value(area_field_names)
 
         qs = cls.objects\
