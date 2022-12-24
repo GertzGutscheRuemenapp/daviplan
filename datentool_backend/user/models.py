e3a4cf3e1ad19495face608617ab5989f2cc6b5d
Max Bohnet <bohnet@ggr-planung.de>
2022-12-24 02:59:24 +0100

    remove reference to profile in all logging.handlers

--- datentool_backend/user/models.py
+++ datentool_backend/user/models.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -19,6 +21,19 @@ class Profile(DatentoolModelMixin, models.Model):
 
     def __str__(self) -> str:
         return f'{self.__class__.__name__}: {self.user.username}'
+
+    def delete(self, using=None, keep_parents=False, use_protection=False):
+        """remove profile from LogHandler before deleting it"""
+        for hdlr_ref in logging._handlerList:
+            hdlr = hdlr_ref()
+            if hasattr(hdlr, 'user') and hdlr.user == self:
+                del(hdlr.user)
+            if hasattr(hdlr, 'profile') and hdlr.profile == self:
+                del(hdlr.profile)
+
+        super().delete(using=using,
+                       keep_parents=keep_parents,
+                       use_protection=use_protection)
 
 
 @receiver(post_save, sender=User)
