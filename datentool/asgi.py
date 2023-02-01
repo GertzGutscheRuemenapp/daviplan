3312f39fb23a620a3b0b75a760c84918804d5eb1
Christoph Franke <franke@ggr-planung.de>
2023-02-01 11:43:52 +0100

    wrong settings file in asgi config

--- datentool/asgi.py
+++ datentool/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datentool.settings_local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datentool.settings')
 django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
